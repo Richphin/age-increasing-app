78f430093a20e35d7dc42ff466697d18d85ab61d
Richie <richphinsalah6@gmail.com>
2021-03-26 18:02:22 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component , useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   const [state,setstate]= useState({age: 30})
+
+//   const reduceage = () =>{
+//     let newage = state.age - 1;
+//     setstate({ age:newage})
+//   }
+//   const increaseage = () =>{
+//     let newage = state.age + 1;
+//     setstate({age:newage})
+//   }
+//   return (
+//     <div>
+//       <h1>Age is {state.age}</h1>
+//       <botton onClick={reduceage}><h1>-</h1></botton>
+//       <botton onClick={increaseage}><h1>+</h1></botton>
+//     </div>
+//   );
+// }
+
+class App extends Component{
+    constructor(){
+        super();
+        this.state= {age:18};
+    }
+    increase = () =>{
+        let newage=this.state.age + 1
+        this.setState({age: newage})
+    }
+
+
+    render(){
+      return(
+        <div>
+        <h1>age is {this.state.age} years</h1>
+    <input type="button" value="increase age +1" onClick={this.increase}></input>
+   </div>
+      )
+    }
+
 }
 
 export default App;
